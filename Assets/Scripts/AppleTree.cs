a97b10bd651b686c135781c8599df49c01900ded
Richie Nguyen <richieytl@gmail.com>
2017-06-13 20:17:42 -0400

    Create content for the Apple, ApplePicker, and AppleTree scripts.
    Apple script is able to detect when it is to be destroyed when it touches the bottom of the playzone. ApplePicker script is connected to the camera and contains the management of the game by managing known apples and deleting them on failure, and managing the number of baskets and deleting them on failure. AppleTree script calls the methods DropApple every second after two seconds of starting and updates to move the tree every frame with a random chance of 2% every 1/50th second.

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class AppleTree : MonoBehaviour {
 
+	public GameObject applePrefab;
+	public float speed = 1f;
+	public float leftAndRightEdge = 10f;
+	public float chanceToChangeDirections = .02f;
+	public float secondsBetweenAppleDrops = 1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		//Dropping apples every second
+		InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Basic movement and changing directions
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);
+		} else if (pos.x < leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);
+		}
+	}
+
+	void FixedUpdate(){
+		//Changing direction randomly 50 times a second due to FixUpdate
+		if (Random.value < chanceToChangeDirections) {
+			speed *= -1;
+		}
+	}
+
+	void DropApple(){
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
 	}
 }
